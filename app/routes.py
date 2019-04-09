90ba17f25173a907271527d628bbbb3688f4aad4
bitofdon <bitdon@protonmail.com>
2019-04-09 13:11:38 -0500

    added and imported routes skeleton

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from app import app
+
+
+
+#home page
+@app.route('/')
+@app.route('/index')
+def index():
+    #code here
+    #...
+    #return render_template('index.html', ..., ...)
+
+#blog page
+@app.route('/blog')
+def blog():
+    #code here
+    #...
+    #return render_template('blog.html', ..., ...)
+
+
+#login page
+@app.route('/login')
+    #code here
+    #...
+    #return render_template('login.html', ..., ...)
