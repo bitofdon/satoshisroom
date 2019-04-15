9debf0413037096c0e9a416ef9117b3c309a0159
bitofdon <bitdon@protonmail.com>
2019-04-15 10:02:32 -0500

    updated index.html, routes.py - created fake user for testing and imported flash, redirect, and url_for. imported LoginForm to get login page functionality started. Added config.py and css file.

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,31 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
-
+from app.forms import LoginForm
 
 
 #home page
 @app.route('/')
 @app.route('/index')
 def index():
-        #code here
-        #...
-    return render_template('index.html')
+    #fake user for now for testing purposes
+    user = {'username': 'Satoshi'}
+    #fake object for testing purposes - make fake list of blog posts
+    posts = [
+        {
+            'author': {'username':'Satoshi'},
+            'body': 'This is a sample post'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-login page
-@app.route('/login')
+#login page
+@app.route('/login', methods=['GET', 'POST'] )
 def login():
-   #code here..
-   return render_template('login.html')
-
-
-
-#blog page
-# @app.route('/blog')
-# def blog():
-#     #code here..
-#     return render_template('blog.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        #flash a message to the user
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data ))
+        return redirect(url_for('index') )
+    return render_template('login.html', title='Sign In', form=form)
