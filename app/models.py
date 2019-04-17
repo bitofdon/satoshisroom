f2eda080316f797bdf76179d7870eab9d00b10f8
bitofdon <bitdon@protonmail.com>
2019-04-17 12:06:47 -0500

    created Post model for blog posts and added the relationship between User & Post model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 #User model and its corresponding table defined
@@ -7,5 +8,22 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    #relationship between User and Post model - ref: https://docs.sqlalchemy.org/en/13/orm/collections.html
+    #provides the list of posts the User created
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+        #backref = relationship-side, adds author attribute to Post model - ex: posts.author (calls a user instance)
+        #lazy = makes the 'posts' be a query instead of a list of posts
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+#For blog posts - import datetime
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #Record who the author is for blog post
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
